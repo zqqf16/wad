b86ab30c847e14817353ffdd3a1628e0970d4fc5
zqqf16 <zqqf16@gmail.com>
2014-03-28 00:08:03 +0800

    实现了上传下载功能

--- wad/app.py
+++ wad/app.py
@@ -5,27 +5,45 @@ import os
 
 import tornado.ioloop
 import tornado.web
+import tornado.options
 import tornado.httpserver
+
+from tornado.options import define, options
 
 from handler import *
 
+define('port', default=8000, help='running port', type=int)
+define('archives', default='archives', help='archives path')
+define('root', default='../example', help='root directory')
+define('host', default='192.168.6.101', help='host address')
+
 class App(tornado.web.Application):
     def __init__(self):
+        archives_path = os.path.join(options.root, options.archives)
+
         handlers = [
-            (r'/', IndexHandler),
+            (r'[/]?', SingleFileHandler, {'filename': 'index.html'}),
+            (r'/manifest.plist', SingleFileHandler, {'filename': 'manifest.plist'}),
+
             (r'/admin[/]?', AdminHandler),
-            (r'/build/(.*)', BuildHandler),
+            (r'/archives/(.*)', tornado.web.StaticFileHandler, {'path': archives_path}),
         ]
 
         settings = {
-            'template_path': os.path.join(os.path.dirname(__file__), "templates"),
-            "static_path": os.path.join(os.path.dirname(__file__), "static"),
+            'template_path': os.path.join(os.path.dirname(__file__), 'templates'),
+            'static_path': os.path.join(os.path.dirname(__file__), 'static'),
         }
 
         tornado.web.Application.__init__(self, handlers, **settings)
 
-if __name__ == '__main__':
+def main():
+    
+    #Load config
+    tornado.options.parse_command_line()
+
     http_server = tornado.httpserver.HTTPServer(App())
-    http_server.listen(8000)
+    http_server.listen(options.port)
     tornado.ioloop.IOLoop.instance().start()
 
+if __name__ == '__main__':
+    main()
--- wad/handler.py
+++ wad/handler.py
@@ -3,10 +3,10 @@
 
 import os
 import tornado.web
+from tornado.options import options
 from io import BytesIO
 
-import ipa
-import settings
+import utils
 from model import Archive
 
 class FakeBuild(object):
@@ -14,13 +14,13 @@ class FakeBuild(object):
         self.manifest = 'http://fake.plist'
         self.name = 'Fake'
 
-class IndexHandler(tornado.web.RequestHandler):
-    def get(self):
-        build = FakeBuild()
-        self.render('index.html', build=build)
+class SingleFileHandler(tornado.web.StaticFileHandler):
+    def initialize(self, filename):
+        super(SingleFileHandler, self).initialize(options.root)
+        self.filename = filename
 
-    def post(self):
-        self.write('post')
+    def get(self):
+        super(SingleFileHandler, self).get(self.filename)
 
 class AdminHandler(tornado.web.RequestHandler):
     def get(self):
@@ -32,27 +32,51 @@ class AdminHandler(tornado.web.RequestHandler):
         if not file_list:
             self.redirect('/admin')
 
-        self.archive_operation(file_list[0]['body'])
-        self.redirect('/admin')
+        content = file_list[0]['body']
 
-    def archive_operation(self, content):
         try:
-            info = ipa.get_info(BytesIO(content))
+            self.parse_info(content)
         except:
+            self.write('Error!')
             return
 
-        file_name = "{name}_{version}_{build}.ipa".format(**info)
-        size = len(content)
-        archive = Archive.create(file_name=file_name, size=size, **info)
-        archive.save()
+        self.save_archive(content)
+        self.generate_manifest()
+        self.generate_index()
 
-        path = os.path.join(settings.archive_dir, file_name)
+        self.redirect('/admin')
+
+    def parse_info(self, content):
+        '''Parse infomations from ipa file content'''
+
+        info = utils.get_info(BytesIO(content))
+
+        info['file_name'] = '{name}_{version}_{build}.ipa'.format(**info)
+        info['archive_url'] = 'archives/' + info['file_name']
+        info['title'] = info['name']
+        info['subtitle'] = '{name} {version}'.format(name=info['name'], version=info['version'])
+        
+        self.info = info
+
+    def save_archive(self, content):
+        path = os.path.join(options.root, options.archives, self.info['file_name'])
+
         with open(path, 'wb') as f:
             f.write(content)
 
-class BuildHandler(tornado.web.RequestHandler):
-    def get(self):
-        self.write('get')
+    def generate_manifest(self):
+        html = self.render_string('manifest.plist', **self.info)
+        path = os.path.join(options.root, 'manifest.plist')
 
-    def post(self):
-        self.write('post')
+        with open(path, 'w') as f:
+            f.write(html)
+
+    def generate_index(self):
+        index_path = os.path.join(options.root, 'index.html')
+        title = self.info['name']
+        description = '{} {}'.format(self.info['version'], self.info['build'])
+
+        html = self.render_string('index.html', host=options.host, title=title, description=description)
+        with open(index_path, 'w') as f:
+            print 'write'
+            f.write(html)
